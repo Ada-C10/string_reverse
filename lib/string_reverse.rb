bc9f462cdc73280c80b62c44cc8366f1fa6af4c1
Alice Hsiao <alice.hsiao87@gmail.com>
2018-09-09 17:02:10 -0700

    Added comments about time and space complexity of written code

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,7 +1,10 @@
 # A method to reverse a string in place.
+# Time Complexity: O(n/2) = O(n)
+# Space Complexity: O(1) since we are reversing the string "in place"
+
 def string_reverse(my_string)
   return nil if my_string == nil
-  
+
   i = 0
   j = my_string.length - 1
 
