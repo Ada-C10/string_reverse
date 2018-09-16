b065ca48830500e9241cdfff5bb0d3cdd9440d2a
Addison Cole <addison.cole85@gmail.com>
2018-09-16 02:17:36 -0700

    finished string reverse passes all tests

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,17 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  if my_string == nil
+    return my_string
+  end
+
+  y = my_string.length - 1
+  x = 0
+  until x > y do
+    container = my_string[x]
+    my_string[x] = my_string[y]
+    my_string[y] = container
+    y -= 1
+    x += 1
+  end
+
 end
