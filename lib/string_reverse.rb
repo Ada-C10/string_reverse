75c3391acb6688135c486e38f1045f2353444047
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-25 16:55:25 -0700

    Tests pass

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,24 @@
+# def swap(a, b)
+#  c = a
+#  a = b
+#  b = c
+# end
+#
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  if my_string.nil?
+    return nil
+  else
+    n = my_string.length
+    i = 0
+    j = n-i-1
+
+    while i < j do
+      memo = my_string[i]
+      my_string[i] = my_string[j]
+      my_string[j] = memo
+      i = i+1
+      j = j-1
+    end
+  end
 end
