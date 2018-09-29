e670c5358f5ce40dbc00113e11b1b564c5089f8b
Madaleine Shields <maddiev.shields@gmail.com>
2018-09-29 13:08:18 -0700

    Add time/space complexities

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,7 +1,7 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
   return nil if my_string == nil
-  
+
   front_index = 0
   back_index = my_string.length - 1
 
@@ -16,3 +16,7 @@ def string_reverse(my_string)
     back_index -= 1
   end
 end
+
+# time complexity: O(n) - Loops through n times (1/2 the length of the array)
+
+# space completity: O(1) - stays constant as the size of the string doesn't change each iteration
