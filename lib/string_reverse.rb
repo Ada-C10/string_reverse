7572b729549f9bbf199898915d589eaa4fddb380
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-10-28 12:42:59 -0700

    add strig reverse method

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,17 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  # my_string << "not implemented"
+
+  if my_string == nil
+    return nil
+  end
+
+  i = 0
+  j = my_string.length - 1
+  while i < j
+    my_string[i], my_string[j] = my_string[j], my_string [i]
+    i += 1
+    j -= 1
+  end
+  return my_string
 end
