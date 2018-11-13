401adf53b303a4afd8c90b375142df532611f070
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-11-13 10:00:02 -0800

    Added big O

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -4,21 +4,19 @@
 #  b = c
 # end
 #
-# A method to reverse a string in place.
+# T: O(n)
+# S: O(1)
 def string_reverse(my_string)
-  if my_string.nil?
-    return nil
-  else
-    n = my_string.length
-    i = 0
-    j = n-i-1
+  return nil if my_string.nil?
+  n = my_string.length
+  i = 0
+  j = n-i-1
 
-    while i < j do
-      memo = my_string[i]
-      my_string[i] = my_string[j]
-      my_string[j] = memo
-      i = i+1
-      j = j-1
-    end
+  while i < j do
+    memo = my_string[i]
+    my_string[i] = my_string[j]
+    my_string[j] = memo
+    i = i+1
+    j = j-1
   end
 end
