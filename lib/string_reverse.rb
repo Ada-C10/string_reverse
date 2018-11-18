3bdc14edcb2b97f71a335f660481725f837777ca
Dionisia Montanez <dmontanez15@gmail.com>
2018-11-18 10:07:45 -0800

    renamed variables to make easier to read

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -3,16 +3,15 @@ def string_reverse(my_string)
   # checks to see if there is a string to reverse
   return nil if my_string == nil || my_string.length == 0
 
-  string = 0 # first character of string
-  reverse_string = my_string.length - 1 # last character of string
+  # renamed variables to make method easier to read.
+  i = 0 # first index of character of string
+  j = my_string.length - 1 # last index of character of string
 
-  while string < reverse_string
-    temp = my_string[string] # swap with temporary variable
-    my_string[string] = my_string[reverse_string]
-    my_string[reverse_string] = temp
-    string += 1
-    reverse_string -= 1
+  while i < j
+    temp = my_string[i] # swap with temporary variable
+    my_string[i] = my_string[j]
+    my_string[j] = temp
+    i += 1
+    j -= 1
   end
-  # return my_string
-  # my_string << "not implemented"
 end
